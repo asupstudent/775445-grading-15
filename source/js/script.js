1cf51bbdd47636037b8e03740a99fad0d818f759
asupstudent <asupstudent@yandex.ru>
2023-06-18 23:42:35 +0300

    Идет не очень.... шапку вроде настроил, слайдер пока нет, возможно времени не хватит (2 день прошел)

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,22 @@
+const mobileMenu = () => {
+  let navMain = document.querySelector('.main-nav');
+  let navToggle = document.querySelector('.main-nav__toggle');
+
+  navMain.classList.remove('main-nav--nojs');
+
+  navToggle.addEventListener('click', function() {
+    if (navMain.classList.contains('main-nav--closed')) {
+      navMain.classList.remove('main-nav--closed');
+      navMain.classList.add('main-nav--opened');
+    } else {
+      navMain.classList.add('main-nav--closed');
+      navMain.classList.remove('main-nav--opened');
+    }
+  });
+};
+
+mobileMenu();
+
 const swiper = new Swiper('.swiper', {
   loop: true,
 
